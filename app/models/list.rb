cf0223e17de06224735870d05e8a85850cbe9a3d
shane haddock <haddocs@tcd.ie>
2026-07-11 17:50:15 +0900

    some frontend done

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,14 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
 
   validates :name, presence:true, uniqueness:true
+
+    def fetch_api
+      escaped_title = CGI.escape(self.name)
+
+      url = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=#{escaped_title}"
+
+      response = Faraday.get(url)
+      data = JSON.parse(response.body)
+      p data
+    end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,14 @@ class Movie < ApplicationRecord
 
   validates :title, presence:true, uniqueness:true
   validates :overview, presence:true
+
+    def fetch_api
+      escaped_title = CGI.escape(self.title)
+
+      url = "http://www.omdbapi.com/?t=#{escaped_title}&apikey=facedef2"
+
+      response = Faraday.get(url)
+      data = JSON.parse(response.body)
+      p data
+    end
 end
